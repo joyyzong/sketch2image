d58a10bde8b07c6a11b5acfaa5e5f23aa6e459c0
joyyzong <49356513+joyyzong@users.noreply.github.com>
2020-04-24 17:20:34 +0800

    Add files via upload
    data processing : make sketchy datasets to A-to-B or B-to-A training mode

---
+++ 2img_append.py
@@ -0,0 +1,56 @@
+import os
+from PIL import Image
+class_name = 'bee'
+image_path = os.path.join('/Users/joyzong/Desktop/Sketchy/rendered_256x256/256x256/photo/tx_000000000000',class_name)
+sketch_path = os.path.join('/Users/joyzong/Desktop/Sketchy/rendered_256x256/256x256/sketch/tx_000000000000',class_name)
+result_path = os.path.join('/Users/joyzong/Desktop/sketchy_v1',class_name,'result')
+ 
+def pinjie():
+    # 获取当前文件夹中所有JPG图像
+    im_list = [os.path.join(image_path,fn) for fn in os.listdir(image_path) if fn.endswith('.png') or fn.endswith('.jpg') ]
+    im_name_list = [fn.split('.') for fn in os.listdir(image_path) if fn.endswith('.png') or fn.endswith('.jpg') ]
+    sk_list = [os.path.join(sketch_path,fn) for fn in os.listdir(sketch_path) if fn.endswith('.png') or fn.endswith('.jpg') ]
+    sk_name_list = [fn.split('-') for fn in os.listdir(sketch_path) if fn.endswith('.png') or fn.endswith('.jpg') ]
+    #print("image list =",im_list)
+    #print("sketch list =",sk_list)
+    print("image name list = ",im_name_list)
+    print("sketch name list = ",sk_name_list)
+     # 图片转化为相同的尺寸
+    ims = []
+    for i in im_list:
+        new_img = Image.open(i).resize((256, 256), Image.BILINEAR)
+        ims.append(new_img)
+    sks = []
+    for i in sk_list:
+        new_img = Image.open(i).resize((256, 256), Image.BILINEAR)
+        sks.append(new_img)
+    print("number of image = ", len(ims))
+    print("number of sketch = ", len(sks))
+
+    # 单幅图像尺寸
+    width, height = ims[0].size
+    mode = ims[0].mode
+
+
+    # 拼接图片,对于每一幅草图都匹配相应的图像
+    for j, im in enumerate(ims):
+        #print(im_name_list[i][0])
+        sk_number = 0
+        for i, sk in enumerate(sks):
+            if im_name_list[j][0] == sk_name_list[i][0]:
+                # 创建空白长图
+                sk_number = sk_number+1
+                result = Image.new(mode, (width * 2, height))
+                result.paste(sk, box=(0,0))
+                result.paste(im, box=(width,0))
+                # 保存图片
+                #print(str(im_name_list[j][0]))
+                result_name = str(sk_name_list[j][0])+'-'+str(sk_number)+'.png'
+                print(result_name)
+                if not os.path.exists(result_path):
+                    os.makedirs(result_path)
+                result.save(os.path.join(result_path,result_name))
+
+ 
+if __name__ == '__main__':
+    pinjie()
